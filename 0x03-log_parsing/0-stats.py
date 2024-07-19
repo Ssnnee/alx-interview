fdefa2cd45f07135891e61731263cef6d4dee94f
Samuel Nandi <nandisamuelsne@gmail.com>
2024-07-19 03:39:17 +0100

    refacor: improve the script

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,71 +1,57 @@
 #!/usr/bin/python3
-"""Module for stats function."""
-
-"""Struture my thinking
-
-The function have to reads stdin line by line and computes metrics.
-
-1. Have to validate the input format wich is "<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>"
-2. The script have to print the following metrics after every 10 lines read from stdin and/or or a keyboard interruption:
-    - Total file size: File size of all the logs
-    - Number of lines by status code in ascending order
-
-To solve this problem I will use the following steps:
-1. Read the input from stdin
-2. Validate the input format
-3. Compute the metrics
-4. Print the metrics
-5. Handle the keyboard interruption
-6. Print the metrics after every 10 lines read from stdin and/or or a keyboard interruption
+"""Module for stats function.
+This script reads stdin line by line and computes metrics
 """
-
 
 import sys
 
 
-def print_metrics(total_file_size, status_codes):
+def print_metrics(total_file_size, status_code_dic):
     """Print the metrics."""
     print("File size: {}".format(total_file_size))
-    for key in sorted(status_codes.keys()):
-        print("{}: {}".format(status_codes[key], key))
+    for key in sorted(status_code_dic.keys()):
+        print("{}: {}".format(key, status_code_dic[key]))
     return
 
-def input_validator(line):
-    """Validate the input format."""
+
+def input_parser(line):
+    """Parse input and return the <status code> and <file size>"""
     try:
         parsed_line = line.split()
         file_size = int(parsed_line[-1])
-        status_code = parsed_line[-2]
-        if len(parsed_line) < 2:
+        status_code = int(parsed_line[-2])
+
+        if (len(line) < 2):
             return (0, None)
         return (file_size, status_code)
     except Exception:
         return (0, None)
 
+
 def stats():
-    """Reads stdin line by line and computes metrics."""
+    """Main funcion of the script"""
     total_file_size = 0
-    status_codes = {}
-    lines_read = 0
+    status_code_dic = {}
+    i = 0
 
     try:
         for line in sys.stdin:
-            lines_read += 1
-            file_size, status_code = input_validator(line)
-            if file_size == 0:
-                continue
+            file_size, status_code = input_parser(line)
+            i += 1
             total_file_size += file_size
-            if status_code in status_codes:
-                status_codes[status_code] += 1
+            if status_code in status_code_dic:
+                status_code_dic[status_code] += 1
             else:
-                status_codes[status_code] = 1
+                status_code_dic[status_code] = 1
+            if i % 10 == 0:
+                print_metrics(total_file_size, status_code_dic)
 
-            if lines_read % 10 == 0:
-                print_metrics(total_file_size, status_codes)
-        print_metrics(total_file_size, status_codes)
+        if i % 10 != 0:
+            print_metrics(total_file_size, status_code_dic)
+
     except KeyboardInterrupt:
-        print_metrics(total_file_size, status_codes)
-        raise
+        print_metrics(total_file_size, status_code_dic)
+        sys.exit(0)
 
 
 __name__ == "__main__" and stats()
