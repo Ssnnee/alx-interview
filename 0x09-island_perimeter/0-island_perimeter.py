a18c81589fc23d4d1b708a2ef26504d2f821c33a
Samuel Nandi <nandisamuelsne@gmail.com>
2024-08-30 10:18:06 +0100

    Refactor for the style requirements

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+""" Island perimeter """
+
 
 def island_perimeter(grid):
+    """ Returns the perimeter of the island described in grid """
+
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
